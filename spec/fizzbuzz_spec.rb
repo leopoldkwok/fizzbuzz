ebd4787f62cd3763ba069bb42ec5521c99e63dcb
leopoldkwok <leopoldkwok@gmail.com>
2014-08-30 23:12:40 +0100

    add files

---
+++ spec/fizzbuzz_spec.rb
@@ -0,0 +1,13 @@
+require 'fizzbuzz'
+
+describe 'FizzBuzz' do
+	context 'knows that a number is divisible by' do
+		it '3' do
+			expect(is_divisible_by_three?(3)).to be_true
+		end
+
+	end
+	
+
+
+end
